dbe5c4617a947be49e2cb42b41c01aedc77914db
hoangan2002 <trandinhhoangan2002@gmail.com>
2023-09-28 17:16:21 +0700

    Lab4

---
+++ Lab4/Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace lab41
+{
+    public delegate void SampleDelegate(int a, int b);
+    class MathOperations
+    {
+        public void Add(int a, int b) {
+            Console.WriteLine("Add Result: {0}", a + b);
+
+        }
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine("Subtract Result: {0}", a - b);
+
+        }
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine("Multiply Result: {0}", a * b);
+
+        }
+        public void Divide(int a, int b)
+        {
+            Console.WriteLine("Divide Result: {0}", a/b);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex");
+            MathOperations m = new MathOperations();
+            SampleDelegate dlgt = m.Add;
+            dlgt += m.Subtract;
+            dlgt += m.Multiply;
+            dlgt += m.Divide;
+            dlgt(91, 10);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab4/Lab4.2/Lab4.2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace lab42
+{
+    public delegate void Event(int balance);
+    public class Account
+    {
+        int balance;
+       public event Event events;
+        public Account() { }
+        public Account(int balance) { this.balance = balance; }
+        public void OnBalnceChanged(int newbalance)
+        {
+            events.Invoke(newbalance);
+        }
+        public void Transaction(int amount)
+        {
+            
+                balance += amount;
+            OnBalnceChanged(balance);
+            
+        }
+    }
+    class Program
+
+    {   public static void Changed(int newbalance)
+        {
+            Console.WriteLine("New Balance: " + newbalance);
+        }
+
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.events += Changed;
+
+            account.Transaction(1000);
+            account.Transaction(1000);
+            Console.ReadLine();
+        }
+    }
+}
